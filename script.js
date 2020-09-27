5d69653ec94bcc64be4f854bc0ec3f61ffb66a72
dodichakill <dodiherlangga91@gmail.com>
2020-09-27 01:47:26 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+            let Counter = React.createClass({
+                getInitialState: function() {
+                    return {count: 0};
+                },
+    
+                tambahClick: function() {
+                    this.setState({count: this.state.count + 1});
+                },
+
+                kurangClick: function() {
+                    this.setState({count: this.state.count - 1});
+                },
+                
+                deleteClick: function() {
+                    this.setState({count: this.state.count * 0});
+                },
+                
+                render: function() {
+                    return (
+                        <div>
+                            <center>
+                            
+                            <div className="Count-container" >
+                                <h1 className="Count">{this.state.count}</h1>
+                            </div>
+                            
+                            
+                            <button onClick={this.kurangClick} className="Kurang"> <span >-</span> </button>
+                            <button onClick={this.tambahClick} className="Tambah"> <span >+</span>  </button>
+                            <button onClick={this.deleteClick} className="Hapus"><span >C</span>  </button>
+                            </center>
+                        </div>
+                    );
+                }
+            });
+            
+            ReactDOM.render(
+                <Counter/>,
+                document.getElementById('app')
+            );
